f6c4f037f8defa66d32ab199b618111cce227fa1
Cody Gregoir <cody.gregoir@student.ehb.be>
2022-10-10 18:41:32 +0200

    Black background rectangle

--- spaceInvader.js
+++ spaceInvader.js
@@ -5,17 +5,12 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let context = canvas.getContext('2d');
 
-function drawSpaceInvader() {
+function drawRect() {
 
 }
+context.lineWidth = 1;
+context.fillStyle = 'black';
 context.beginPath();
-context.strokestyle = "black";
-context.rect(0, 0, 300, 300)
+context.rect(50, 50, 300, 300);
 context.stroke();
-
-context.beginPath();
-context.rect(20, 100, 50, 50);
-context.rect(70, 150, 50, 50);
-context.rect(70, 200, 50, 50);
-context.rect(20, 200, 50, 50);
-context.stroke();
+context.fill();
