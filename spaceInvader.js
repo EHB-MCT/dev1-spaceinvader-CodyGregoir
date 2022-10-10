c3ea643510afdfc93a6527c636b3f66f41c53239
Cody Gregoir <cody.gregoir@student.ehb.be>
2022-10-10 18:44:46 +0200

    Top left rectangle

--- spaceInvader.js
+++ spaceInvader.js
@@ -14,3 +14,9 @@ context.beginPath();
 context.rect(50, 50, 300, 300);
 context.stroke();
 context.fill();
+
+context.lineWidth = 1;
+context.fillStyle = '#66FF00';
+context.beginPath();
+context.rect(75, 150, 50, 50);
+context.fill();
