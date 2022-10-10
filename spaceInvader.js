d6cd05a272da669df33daa20e2c8f3f56e1a936c
Cody Gregoir <cody.gregoir@student.ehb.be>
2022-10-10 17:55:02 +0200

    Background rectangle

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,14 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+function drawSpaceInvader() {
+
+}
+context.beginPath();
+context.strokestyle = "black";
+context.rect(0, 0, 300, 300)
+context.stroke();
