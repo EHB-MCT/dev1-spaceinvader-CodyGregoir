0905b11e53b013fbc79a980d98ccc3930b0b3a06
Cody Gregoir <cody.gregoir@student.ehb.be>
2022-10-10 19:58:40 +0200

    bottom middle right square

--- spaceInvader.js
+++ spaceInvader.js
@@ -44,3 +44,9 @@ context.fillStyle = '#66FF00';
 context.beginPath();
 context.rect(125, 250, 50, 50);
 context.fill();
+
+context.lineWidth = 1;
+context.fillstyle = '#66FF00';
+context.beginPath();
+context.rect(75, 250, 50, 50);
+context.fill();
